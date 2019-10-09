22ce3524654c2adac91af249e50377112f78464c
leost3 <leost3@gmail.com>
2019-10-09 12:20:09 -0400

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+    }
+}
